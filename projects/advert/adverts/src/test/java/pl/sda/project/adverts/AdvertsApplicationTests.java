b6fc4b484cec760857002b6926ca68f961a76bf4
oleann <67549528+oleann@users.noreply.github.com>
2020-10-11 10:45:42 +0200

    Add files via upload

---
+++ projects/advert/adverts/src/test/java/pl/sda/project/adverts/AdvertsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.project.adverts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
